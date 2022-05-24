5d4e5d7e1570a2239d0d26bb6e46d4b1f4639047
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-24 19:48:03 +0200

    php-google-faq

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $answer = [
          <ul><li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
          <li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.</ul>
           <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-          Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
+          Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</ul>"
         ],
      [ 'DOMANDA'=>"Stabilire il paese associato al tuo account",
           'RISPOSTA'=>"Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
